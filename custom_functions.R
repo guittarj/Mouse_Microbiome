4a5128d97d76ed7e63a1bd34ca2f8a7592643414
Unknown <guittarj@gmail.com>
2018-07-11 13:41:59 -0400

    update

--- custom_functions.R
+++ custom_functions.R
@@ -1,5 +1,87 @@
 # Custom functions for microbiome analyses. Created Feb 28, 2017.
 
+bac_extract <- function(x, trait) {
+  
+  # for each species query result, filter blanks and pick best value
+  for(i in 1:length(tax_bacdat)) {
+    names(x[[i]]) <- tax_bacdat[[i]]
+    x[[i]] <- x[[i]][sapply(x[[i]], length) > 0]
+    x[[i]] <- sapply(x[[i]], pick_best)
+  }
+  
+  # unlist, clean, return df
+  x <- unlist(x)
+  x[x == 'NULL'] <- NA
+  x_names <- strsplit(names(x), '.', fixed = TRUE)
+  x <- data.frame(
+    Genus = sapply(x_names, function(y) y[[1]]),
+    Species = sapply(x_names, function(y) y[[2]]),
+    trait = trait,
+    val = x
+  )
+  x <- x[!is.na(x$val), ]
+  x$val <- as.character(x$val)
+  return(x)
+}
+
+bac_search <- function (x) {
+  # needs list of genera to search for
+  
+  #just use the first page of results
+  page <- '1'
+  taxon <- 'https://bacdive.dsmz.de/api/bacdive/taxon/'
+  
+  for (i in 1:length(x)) {
+    
+    url_genus <- URLencode(paste0(taxon, x[i], '/?page=', page, '&format=json'))
+    response <- getURL(url_genus, userpwd="guittarj@msu.edu:twkYkcJbxCQzEvwkUi", httpauth = 1L)
+    jsondata <- fromJSON(response)
+    urls <- unlist(jsondata$results)
+    
+    if (length(urls) > 0) {
+      genus <- x[i] 
+      bacdat[[genus]] <- list()
+      for (j in 1:length(urls)) {
+        urlx <- URLencode(paste0(urls[j],'?&format=json'))
+        response <- getURL(urlx, userpwd="guittarj@msu.edu:twkYkcJbxCQzEvwkUi", httpauth = 1L)
+        jsondata <- fromJSON(response)
+        bacdat[[genus]][[j]] <- jsondata
+      }
+    }
+    print(paste("Downloaded", length(urls), "entries for", x[i]))
+  }
+}
+
+
+bac_search_ID <- function (x, bacdat = list()) {
+  
+  #just use the first page of results
+  page <- '1'
+  bacURL <- 'https://bacdive.dsmz.de/api/bacdive/'
+  
+  for (i in x) {
+    
+    url_bacID <- URLencode(paste0(bacURL, 'bacdive_id/', i, '/?page=', page, '&format=json'))
+    response <- getURL(url_bacID, userpwd="guittarj@msu.edu:twkYkcJbxCQzEvwkUi", httpauth = 1L)
+    bacdat[[i]] <- fromJSON(response)
+    
+    prog <- match(i, x)
+    if(match(i, x) %% 500 == 0) {
+      
+      #Print status
+      print(paste("Downloaded", match(i, x), "of", length(x), "entries"))
+      
+      #save progress
+      saveRDS(bacdat, file = paste0('bacdat', match(i, x), '.RDS'))
+    }
+  
+  }
+  
+  return(bacdat)
+
+}
+
+
 loadpax <- function(pkg){
   # (1) checks package installation, (2) installs them if not, then (3) loads them
     new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
@@ -81,9 +163,14 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   }
 }
 
+#suppress warnings as numeric
+swan <- function (x) suppressWarnings(as.numeric(x))
+
 # print list of loaded functions
 print(data.frame(Custom_Functions = 
-  c('loadpax: install+load multiple packages',
+  c('bac_search: search bacdat',
+  	'bac_extract: extract data from bacdat object',
+  	'loadpax: install+load multiple packages',
     'grid_arrange_shared_legend: Multiple plots, one legend',
-    'multiplot: multiple plots')))
-
+    'multiplot: multiple plots',
+    'swan: a function to suppress warnings, convert to numeric')))
